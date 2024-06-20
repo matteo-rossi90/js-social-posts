9a7688e88ac686e4a10da6a314561124ba589a4f
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-20 15:22:57 +0200

    feat: add js code to show in DOM the posts list

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.*/
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Individuare il contenitore dove è collocato il post in HTML
+const container = document.querySelector("#container");
+
 //Array contenente l'elenco di oggetti che corrispondono ai post
 const posts = [
     {
@@ -65,3 +68,51 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+//Estrarre gli oggetti dell'array
+posts.forEach((post) =>{
+
+    //estrarre le proprietà degli array
+    for(let key in post){
+        console.log(key, " : ", post[key]);
+        
+    }
+
+    //creare gli elementi di markup
+    let markupElement = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="${post.author.name}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    `;
+
+    //stampare in DOM i vari post
+    container.innerHTML += markupElement;
+});
